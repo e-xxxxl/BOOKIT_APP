f1fa698f434b37090494e939f4ab5dea02474fdb
emmanuel <eajejohnson@gmail.com>
2025-09-19 00:41:43 +0100

    Refactored CORS configuration to specify allowed origins, methods, and headers

--- BACKENDHOS/index.js
+++ BACKENDHOS/index.js
@@ -11,7 +11,12 @@ const mongoose = require("mongoose")
 
 app.use(express.urlencoded({ extended: true, limit: "100mb" }))
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use("/user", userRouter)
 app.use("/api", hotelRouter)
 require("dotenv").config()
